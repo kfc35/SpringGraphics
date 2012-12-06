39ee21f500baf7d51b6eb3d82acec24c5cdd8bce
Kevin Chen <chen.kevin.f@gmail.com>
2012-12-06 01:16:15 -0500

    finished glass

--- PA3B/src/ray/material/Glass.java
+++ PA3B/src/ray/material/Glass.java
@@ -51,7 +51,7 @@ public class Glass extends Material {
 	public RayRecord[] getIncomingSpecularRays(IntersectionRecord record, Vector3 outgoing) {
 		// TODO(B): Fill in this method.
 
-		// --- copied from Glazed -- calculate n_1, n_2, fresnel
+		// --- copied from Glazed -- need all the variables!
 		Vector3 outgoingNorm = new Vector3(outgoing);
 		outgoingNorm.normalize();
 		
@@ -83,8 +83,66 @@ public class Glass extends Material {
 		//cos(theta) = (normalNorm dot outgoingNorm) / (||normalNorm|| ||outgoingNorm||)
 		double fresnel = r_0 + (1.0 - r_0) * Math.pow(1.0 - projection, 5.0);
 		
+		//Reflected Ray = 2 * normalNorm * (normalNorm dot outgoingNorm) - outgoingNorm
+		Vector3 reflectedRay = new Vector3(normalNorm);
+		reflectedRay.scale(2.0);
+		reflectedRay.scale(projection);
+		reflectedRay.sub(outgoingNorm);
+		
 		// --- end copy
 		
-		return null;
+		//Check for total internal reflection
+		//Taken from Lecture 35 Slide 27
+		//TODO outgoing may have to be reversed... this will be a bug if it arises.
+		double determinant = 1 - (Math.pow(n_1, 2.0) * (1 -Math.pow(projection, 2.0)))/(Math.pow(n_2, 2.0));
+		
+		RayRecord[] toReturn = null;
+		if (determinant < 0) {
+			toReturn = new RayRecord[1];
+			
+		}
+		else {
+			toReturn = new RayRecord[2];
+		}
+		
+		//avoid c/ping this part of the code multiple times
+		toReturn[0] = new RayRecord();
+		toReturn[0].ray.start = 0;
+		toReturn[0].ray.end = Double.POSITIVE_INFINITY;
+		//starts at the intersection point
+		toReturn[0].ray.set(record.location, reflectedRay);
+		
+		if (determinant < 0) {
+			//For total internal reflection, its factor is 1.0
+			toReturn[0].factor.set(1.0);
+		}
+		else {
+			toReturn[0].factor.set(fresnel);
+			
+			//Calculate transmitted vector
+			//Taken from Lecture 35 Slide 27
+			//TODO may need to reverse outgoingNorm!
+			Vector3 transmittedVector = new Vector3(outgoingNorm);
+			Vector3 subtractArgOne = new Vector3(normalNorm);
+			Vector3 subtractArgTwo = new Vector3(normalNorm);
+			
+			//TODO may need to reverse outgoingNorm!
+			subtractArgOne.scale(projection);
+			transmittedVector.sub(subtractArgOne);
+			transmittedVector.scale(n_1/n_2);
+			
+			subtractArgTwo.scale(Math.sqrt(determinant));
+			transmittedVector.sub(subtractArgTwo);
+			
+			toReturn[1] = new RayRecord();
+			toReturn[1].ray.start = 0;
+			toReturn[1].ray.end = Double.POSITIVE_INFINITY;
+			//starts at the intersection point as well
+			toReturn[1].ray.set(record.location, transmittedVector);
+			
+			toReturn[1].factor.set(1.0 - fresnel);
+		}
+		
+		return toReturn;
 	}
 }
